1f1048252c9ad739182144bcf03d73f763c44dc0
kateChernysheva1 <116594774+kateChernysheva1@users.noreply.github.com>
2023-05-21 00:11:08 +0200

    h4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,63 +1,81 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { Feedback } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
 
-export class App extends Component {
-  state = {
+export function App() {
+  const [state, updateState] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
+  });
+
+  const getKeys = () => {
+    return Object.keys(state);
   };
 
-  getKeys = () => {
-    return Object.keys(this.state);
+  const countTotalFeedback = () => {
+    return Object.values(state).reduce((ac, el) => ac + el, 0);
   };
 
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((ac, el) => ac + el, 0);
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const total = this.countTotalFeedback();
-    if (total) return Math.round((this.state.good / total) * 100);
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    if (total) return Math.round((state.good / total) * 100);
     else return 0;
   };
 
-  onLeaveFeedback = e => {
+  const onLeaveFeedback = e => {
     const { name } = e.target;
-    this.setState(prevState => ({
-      [name]: prevState[name] + 1,
+
+    let goodStart = 0;
+    let neutralStart = 0;
+    let badStart = 0;
+
+    switch (name) {
+      case 'good':
+        goodStart += 1;
+        break;
+      case 'neutral':
+        neutralStart += 1;
+        break;
+      case 'bad':
+        badStart += 1;
+        break;
+
+      default:
+        console.warn('Нет имени!');
+        break;
+    }
+
+    updateState(prev => ({
+      good: prev.good + goodStart,
+      neutral: prev.neutral + neutralStart,
+      bad: prev.bad + badStart,
     }));
   };
 
-  render() {
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <Feedback
-            options={this.getKeys()}
-            onLeaveFeedback={this.onLeaveFeedback}
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <Feedback options={getKeys()} onLeaveFeedback={onLeaveFeedback} />
+      </Section>
+
+      <Section title="Statistics">
+        {countTotalFeedback() !== 0 && (
+          <Statistics
+            good={state.good}
+            neutral={state.neutral}
+            bad={state.bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
-        </Section>
+        )}
 
-        <Section title="Statistics">
-          {this.countTotalFeedback() !== 0 && (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          )}
-
-          {this.countTotalFeedback() === 0 && (
-            <Notification message="There is no feedback"></Notification>
-          )}
-        </Section>
-      </>
-    );
-  }
+        {countTotalFeedback() === 0 && (
+          <Notification message="There is no feedback"></Notification>
+        )}
+      </Section>
+    </>
+  );
 }
